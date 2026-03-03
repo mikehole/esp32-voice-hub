8de7ef37b3f1a2867455893c93fc7d20da974e5c
Minerva <minerva@prodigi.com>
2026-03-03 22:10:26 +0000

    Use full-height LVGL buffer to prevent tearing/banding

--- firmware/platformio/src/lcd_config.h
+++ firmware/platformio/src/lcd_config.h
@@ -15,7 +15,8 @@
 #define EXAMPLE_PIN_NUM_LCD_RST     21
 #define EXAMPLE_PIN_NUM_BK_LIGHT    47
 
-#define EXAMPLE_LVGL_BUF_HEIGHT        (EXAMPLE_LCD_V_RES / 10)
+// Full-height buffer to prevent tearing (uses more RAM but eliminates banding)
+#define EXAMPLE_LVGL_BUF_HEIGHT        EXAMPLE_LCD_V_RES
 #define EXAMPLE_LVGL_TICK_PERIOD_MS    2                          //Timer time
 #define EXAMPLE_LVGL_TASK_MAX_DELAY_MS 500                        //LVGL Indicates the maximum time for a task to run
 #define EXAMPLE_LVGL_TASK_MIN_DELAY_MS 1                          //LVGL Minimum time to run a task
